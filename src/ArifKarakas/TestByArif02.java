2218fd1ccddcaee8a2e3b88554bde63c2a54b27b
ArifKarakas <arifkarakas@gmail.com>
2022-07-31 22:45:01 +0300

    My third work

---
+++ src/ArifKarakas/TestByArif02.java
@@ -0,0 +1,14 @@
+package ArifKarakas;
+
+public class TestByArif02 {
+    public static void main(String[] args) {
+
+        sumWords("Hello", "Java");
+    }
+
+    public static void sumWords(String word1, String word2){
+
+        System.out.println(word1+word2);
+
+    }
+}
